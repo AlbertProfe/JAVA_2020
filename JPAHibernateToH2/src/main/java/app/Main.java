9099c43e07296905d9d8d5d7421cbd8b040094f6
AlbertProfe <albertprofe@gmail.com>
2020-11-03 19:46:46 +0100

    commit Author and Book oneToMany @oneToMany

--- JPAHibernateToH2/src/main/java/app/Main.java
+++ JPAHibernateToH2/src/main/java/app/Main.java
@@ -25,10 +25,10 @@ public class Main {
 		Author leon = new Author("Leon Tolstoi", "Russia");
 		Author victor = new Author("Victor Hugo", "French");
 		Author dante = new Author("Dante Alighieri", "Italy");
-		
-		 virginia.addBook(new Book ("To the lighthouse"));
-		 virginia.addBook(new Book ("Orlando"));
-		 virginia.addBook(new Book ("A room of my own"));
+
+		virginia.addBook(new Book("To the lighthouse"));
+		virginia.addBook(new Book("Orlando"));
+		virginia.addBook(new Book("A room of my own"));
 
 		Optional<Author> savedAuthor1 = authorRepository.save(virginia);
 		Optional<Author> savedAuthor2 = authorRepository.save(leon);
@@ -49,42 +49,41 @@ public class Main {
 		Optional<Author> authorByName = authorRepository.findByName("Leon Tolstoi");
 		System.out.println("Searching for an author by name: ");
 		authorByName.ifPresent(System.out::println);
-		
+
 		// Find author by name and delete it, that is Leon Tolstoi
 		authorByName = authorRepository.deleteByName("Leon Tolstoi");
 		authorByName.ifPresent(System.out::println);
-		
+
 		// Search for a book by ID 1, that is "To the lighthouse"
-        Optional<Book> foundBook = bookRepository.findById(1);
-        foundBook.ifPresent(System.out::println);
-        
-        // Search for a book with an invalid ID
-        Optional<Book> notFoundBook = bookRepository.findById(5);
-        notFoundBook.ifPresent(System.out::println);
-        
-        // List all books
-        List<Book> books = bookRepository.findAll();
-        System.out.println("Books in database:");
-        books.forEach(System.out::println);
-        
-        // Find a book by name, so "Orlando"
-        Optional<Book> queryBook2 = bookRepository.findByTitle("Orlando");
-        System.out.println("Query for book 2:");
-        queryBook2.ifPresent(System.out::println);
-        
-        // Find a book by name using a named query, that is "A room of my own"
-        Optional<Book> queryBook3 = bookRepository.findByNameNamedQuery("A room of my own");
-        System.out.println("Query for book 3:");
-        queryBook3.ifPresent(System.out::println);
-        
-        // Add a book to author Virginia Wolf, the #4 so "Mrs Daloway"
-        Optional<Author> bookToAuthor = authorRepository.findById(1);
-        bookToAuthor.ifPresent(bookToAdd -> {
-        	bookToAdd.addBook(new Book("Mrs Daloway"));
-            System.out.println("Saved author: " + authorRepository.save(bookToAdd));
-        });
-		
-		
+		Optional<Book> foundBook = bookRepository.findById(1);
+		foundBook.ifPresent(System.out::println);
+
+		// Search for a book with an invalid ID
+		Optional<Book> notFoundBook = bookRepository.findById(5);
+		notFoundBook.ifPresent(System.out::println);
+
+		// List all books
+		List<Book> books = bookRepository.findAll();
+		System.out.println("Books in database:");
+		books.forEach(System.out::println);
+
+		// Find a book by name, so "Orlando"
+		Optional<Book> queryBook2 = bookRepository.findByTitle("Orlando");
+		System.out.println("Query for book 2:");
+		queryBook2.ifPresent(System.out::println);
+
+		// Find a book by name using a named query, that is "A room of my own"
+		Optional<Book> queryBook3 = bookRepository.findByNameNamedQuery("A room of my own");
+		System.out.println("Query for book 3:");
+		queryBook3.ifPresent(System.out::println);
+
+		// Add a book to author Virginia Wolf, the #4 so "Mrs Daloway"
+		Optional<Author> bookToAuthor = authorRepository.findById(1);
+		bookToAuthor.ifPresent(bookToAdd -> {
+			bookToAdd.addBook(new Book("Mrs Daloway"));
+			System.out.println("Saved author: " + authorRepository.save(bookToAdd));
+		});
+
 		// Close the entity manager and associated factory
 		entityManager.close();
 		entityManagerFactory.close();
