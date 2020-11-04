d9820148c284435dc3971fc76472c4f562095862
admin CIFO <admin CIFO@DESKTOP-NG7OTN7>
2020-11-04 10:42:59 +0100

    Commit optional and create authors

--- JPAHibernateToH2/src/main/java/app/Main.java
+++ JPAHibernateToH2/src/main/java/app/Main.java
@@ -8,46 +8,43 @@ import repository.AuthorRepository;
 import java.util.List;
 import java.util.Optional;
 
-
 public class Main {
-    public static void main(String[] args) {
-        // Create our entity manager
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("library");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        
-        // Create our repositories ...
-        AuthorRepository authorRepository = new AuthorRepository(entityManager);
-        // Create an author
-       // Author virginia = new Author("Virginia Wolf", "UK");
-        //Author leon = new Author("Leon Tolstoi", "Russia");
-       // Author victor = new Author("Victor Hugo", "French");
-       // Author dante = new Author("Dante Alighieri", "Italy");
-        
-       // Optional<Author> savedAuthor1 = authorRepository.save(virginia);
-      //  Optional<Author> savedAuthor2 = authorRepository.save(leon);
-      //  Optional<Author> savedAuthor3 = authorRepository.save(victor);
-      //  Optional<Author> savedAuthor4 = authorRepository.save(dante);
-        
-      //  System.out.println("Saved author: " + savedAuthor1.get());
-        // Find all authors
-        List<Author> authors = authorRepository.findAll();
-        System.out.println("Authors:");
-        for (Author authorToPrint : authors)
-        {
-        	System.out.println(authorToPrint);
-        }
-        	
-        	
-        authors.forEach(System.out::println);
-        
-        // Find author by name
-        //Optional<Author> authorByName = authorRepository.findByName("Leon Tolstoi");
-        //System.out.println("Searching for an author by name: ");
-       // authorByName.ifPresent(System.out::println);
-        Optional<Author> authorByName = authorRepository.deleteByName("Leon Tolstoi");
-        authorByName.ifPresent(System.out::println);
-        // Close the entity manager and associated factory
-        entityManager.close();
-        entityManagerFactory.close();
-    }
+	public static void main(String[] args) {
+		// Create our entity manager
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("library");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+		// Create our repositories ...
+		AuthorRepository authorRepository = new AuthorRepository(entityManager);
+		// Create an author
+		Author virginia = new Author("Virginia Wolf", "UK");
+		Author leon = new Author("Leon Tolstoi", "Russia");
+		Author victor = new Author("Victor Hugo", "French");
+		Author dante = new Author("Dante Alighieri", "Italy");
+
+		Optional<Author> savedAuthor1 = authorRepository.save(virginia);
+		Optional<Author> savedAuthor2 = authorRepository.save(leon);
+		Optional<Author> savedAuthor3 = authorRepository.save(victor);
+		Optional<Author> savedAuthor4 = authorRepository.save(dante);
+
+		// System.out.println("Saved author: " + savedAuthor1.get());
+		// Find all authors
+		List<Author> authors = authorRepository.findAll();
+		System.out.println("Authors:");
+		for (Author authorToPrint : authors) {
+			System.out.println(authorToPrint);
+		}
+
+		authors.forEach(System.out::println);
+
+		// Find author by name
+		Optional<Author> authorByName = authorRepository.findByName("Leon Tolstoi");
+		System.out.println("Searching for an author by name: ");
+		authorByName.ifPresent(System.out::println);
+		authorByName = authorRepository.deleteByName("Leon Tolstoi");
+		authorByName.ifPresent(System.out::println);
+		// Close the entity manager and associated factory
+		entityManager.close();
+		entityManagerFactory.close();
+	}
 }
