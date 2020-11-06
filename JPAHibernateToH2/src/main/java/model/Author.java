8d345f97878ea6d25d369f1c891bf80e695c7937
admin CIFO <admin CIFO@DESKTOP-NG7OTN7>
2020-11-06 14:10:01 +0100

    commit controller 3

--- JPAHibernateToH2/src/main/java/model/Author.java
+++ JPAHibernateToH2/src/main/java/model/Author.java
@@ -1,6 +1,5 @@
 package model;
 
-
 import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.CascadeType;
--- JPAHibernateToH2/src/main/java/repository/BookRepository.java
+++ JPAHibernateToH2/src/main/java/repository/BookRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 public class BookRepository {
+	
 	private EntityManager entityManager;
 
 	public BookRepository(EntityManager entityManager) {
